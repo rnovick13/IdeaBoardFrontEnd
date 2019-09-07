1e75b256751db29259bb6da3cd4b4897797da2d7
rnovick13 <rnovick13@gmail.com>
2019-09-07 15:31:06 -0400

    fetch works and state updates

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 class IdeasContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ideas: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/ideas')
+    .then(response => response.json())
+    .then(idea => this.setState({ideas: idea}))
+  }
+
   render() {
     return (
       <div>
